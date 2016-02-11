b6f6333e680ba6ecde88a719755c6b4c1ce8ba8b
Andreas Ruppen <ruppena@MacBook.local>
2016-02-11 22:27:21 +0100

    Use a hash value for the guid

--- FeedCreator/feedcreator.py
+++ FeedCreator/feedcreator.py
@@ -22,6 +22,7 @@ import mimetypes
 import youtube_dl
 import json
 import glob
+import hashlib
 import re
 from rfeed import *
 
@@ -123,7 +124,7 @@ class Tube4Droid:
                     link=self.__serveruri+filename,
                     description=description,
                     author=author,
-                    guid=Guid(self.__serveruri+re.sub('\d+_', '', filename)),
+                    guid=Guid(hashlib.sha512(self.__serveruri+re.sub('\d+_', '', filename)).hexdigest()),
                     pubDate=datetime.datetime(int(uploaddate[0:4]), int(uploaddate[4:6]), int(uploaddate[6:8]), 10, 00),
                     categories=tags,
                     extensions = [itunes_item],
